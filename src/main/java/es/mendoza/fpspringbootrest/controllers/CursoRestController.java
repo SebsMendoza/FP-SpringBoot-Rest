9304ea9951665ac9889491e1993a3c9579b34bb9
Sebastian Mendoza <jhoansebastian.mendoza@alumno.iesluisvives.org>
2022-05-26 12:47:02 +0200

    Iniciando datos

--- src/main/java/es/mendoza/fpspringbootrest/controllers/CursoRestController.java
+++ src/main/java/es/mendoza/fpspringbootrest/controllers/CursoRestController.java
@@ -1,10 +1,16 @@
 package es.mendoza.fpspringbootrest.controllers;
 
+import es.mendoza.fpspringbootrest.models.Curso;
 import es.mendoza.fpspringbootrest.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/curso")
@@ -15,5 +21,15 @@ public class CursoRestController {
     @GetMapping("/prueba")
     public String test() {
         return "Prueba correcta";
+    }
+
+    @GetMapping("/cursos")
+    public ResponseEntity<List<Curso>> findAll() {
+        List<Curso> cursos = cursoRepository.findAll();
+        if (cursos.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No hay cursos encontrados");
+        } else {
+            return ResponseEntity.ok(cursos);
+        }
     }
 }
