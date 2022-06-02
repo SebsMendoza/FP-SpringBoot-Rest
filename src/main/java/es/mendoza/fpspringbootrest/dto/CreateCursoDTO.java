74984ffa57e7bd9b7b0660898fcbb4cceaca17fe
Sebastian Mendoza <jhoansebastian.mendoza@alumno.iesluisvives.org>
2022-06-02 19:31:29 +0200

    Cambios generales II

---
+++ src/main/java/es/mendoza/fpspringbootrest/dto/CreateCursoDTO.java
@@ -0,0 +1,11 @@
+package es.mendoza.fpspringbootrest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateCursoDTO {
+    private String nombre;
+    private String siglas;
+}
