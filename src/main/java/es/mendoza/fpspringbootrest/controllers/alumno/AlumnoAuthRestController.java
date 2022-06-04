7efa3522b50cb23546a890f456633c9aa1b017a8
Sebastian Mendoza <jhoansebastian.mendoza@alumno.iesluisvives.org>
2022-06-04 21:56:53 +0200

    Nuevas clases e implantación de seguridad II

---
+++ src/main/java/es/mendoza/fpspringbootrest/controllers/alumno/AlumnoAuthRestController.java
@@ -0,0 +1,183 @@
+package es.mendoza.fpspringbootrest.controllers.alumno;
+
+import es.mendoza.fpspringbootrest.config.APIConfig;
+import es.mendoza.fpspringbootrest.dto.alumnos.CreateAlumnoDTO;
+import es.mendoza.fpspringbootrest.dto.alumnos.ListAlumnoPageDTO;
+import es.mendoza.fpspringbootrest.errors.GeneralBadRequestException;
+import es.mendoza.fpspringbootrest.errors.alumnos.AlumnoBadRequestException;
+import es.mendoza.fpspringbootrest.errors.alumnos.AlumnoNotFoundException;
+import es.mendoza.fpspringbootrest.errors.alumnos.AlumnosNotFoundException;
+import es.mendoza.fpspringbootrest.mapper.AlumnoMapper;
+import es.mendoza.fpspringbootrest.models.Alumno;
+import es.mendoza.fpspringbootrest.repositories.AlumnoRepository;
+import es.mendoza.fpspringbootrest.service.uploads.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(APIConfig.API_PATH + "/auth/alumno")
+public class AlumnoAuthRestController {
+    private final AlumnoRepository alumnoRepository;
+    private final StorageService storageService;
+    private final AlumnoMapper alumnoMapper;
+
+    @Autowired
+    public AlumnoAuthRestController(AlumnoRepository alumnoRepository, StorageService storageService, AlumnoMapper alumnoMapper) {
+        this.alumnoRepository = alumnoRepository;
+        this.storageService = storageService;
+        this.alumnoMapper = alumnoMapper;
+    }
+
+    @CrossOrigin(origins = "http://localhost:7575")
+
+    //Obtenemos todos los alumnos
+    @GetMapping("/")
+    public ResponseEntity<?> findAll(@RequestParam(name = "limit") Optional<String> limit, @RequestParam(name = "nombre") Optional<String> nombre) {
+        List<Alumno> alumnos = null;
+        try {
+            if (nombre.isPresent()) {
+                alumnos = alumnoRepository.findByNombreContainsIgnoreCase(nombre.get());
+            } else {
+                alumnos = alumnoRepository.findAll();
+            }
+            if (limit.isPresent() && !alumnos.isEmpty() && alumnos.size() > Integer.parseInt(limit.get())) {
+                return ResponseEntity.ok(alumnoMapper.toDTO(alumnos.subList(0, Integer.parseInt(limit.get()))));
+            } else {
+                if (!alumnos.isEmpty()) {
+                    return ResponseEntity.ok(alumnoMapper.toDTO(alumnos));
+                } else {
+                    throw new AlumnosNotFoundException();
+                }
+            }
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Selección de datos", "Parámetros de consulta incorrectos");
+        }
+    }
+
+    //Obtenemos un alumno por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Alumno alumno = alumnoRepository.findById(id).orElse(null);
+        if (alumno == null) {
+            throw new AlumnoNotFoundException(id);
+        } else {
+            return ResponseEntity.ok(alumnoMapper.toDTO(alumno));
+        }
+    }
+
+    //Insertar alumno
+    @PostMapping("/")
+    public ResponseEntity<?> save(@RequestBody CreateAlumnoDTO alumnoDTO) {
+        try {
+            Alumno alumno = alumnoMapper.fromDTO(alumnoDTO);
+            checkAlumnoData(alumno);
+            Alumno alumnoInsertado = alumnoRepository.save(alumno);
+            return ResponseEntity.ok(alumnoMapper.toDTO(alumnoInsertado));
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Insertar", "Error al insertar el alumno. Campos incorrectos");
+        }
+    }
+
+    //Actualizar alumno por id
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Alumno alumno) {
+        try {
+            Alumno alumnoActualizado = alumnoRepository.findById(id).orElse(null);
+            if (alumnoActualizado == null) {
+                throw new AlumnoNotFoundException(id);
+            } else {
+                checkAlumnoData(alumno);
+                alumnoActualizado.setNombre(alumno.getNombre());
+                alumnoActualizado.setCorreo(alumno.getCorreo());
+                alumnoActualizado = alumnoRepository.save(alumnoActualizado);
+                return ResponseEntity.ok(alumnoMapper.toDTO(alumnoActualizado));
+            }
+        } catch (ResponseStatusException e) {
+            throw new GeneralBadRequestException("Actualizar", "Error al actualizar el alumno. CAmpos incorrectos");
+        }
+    }
+
+    //Borrar un alumno
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            Alumno alumno = alumnoRepository.findById(id).orElse(null);
+            if (alumno == null) {
+                throw new AlumnoNotFoundException(id);
+            } else {
+                alumnoRepository.delete(alumno);
+                return ResponseEntity.ok(alumnoMapper.toDTO(alumno));
+            }
+        } catch (ResponseStatusException e) {
+            throw new GeneralBadRequestException("Eliminar", "Error al borrar el alumno");
+        }
+    }
+
+    private void checkAlumnoData(Alumno alumno) {
+        if (alumno.getNombre() == null || alumno.getNombre().isEmpty()) {
+            throw new AlumnoBadRequestException("Nombre", "El nombre es obligatorio");
+        }
+        if (alumno.getCorreo() == null || alumno.getCorreo().isEmpty()) {
+            throw new AlumnoBadRequestException("Correo", "El correo es obligatorio");
+        }
+    }
+
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> nuevoAlumno(@RequestPart("alumno") CreateAlumnoDTO alumnoDTO, @RequestPart("file") MultipartFile file) {
+        String urlImagen = null;
+        try {
+            Alumno alumno = alumnoMapper.fromDTO(alumnoDTO);
+            checkAlumnoData(alumno);
+            if (!file.isEmpty()) {
+                String imagen = storageService.store(file);
+                urlImagen = storageService.getUrl(imagen);
+                alumno.setImagen(urlImagen);
+            }
+            Alumno alumnoInsertado = alumnoRepository.save(alumno);
+            return ResponseEntity.ok(alumnoMapper.toDTO(alumnoInsertado));
+        } catch (AlumnoNotFoundException e) {
+            throw new GeneralBadRequestException("Insertar", "Error al insertar el alumno. Campos incorrectos");
+        }
+    }
+
+    //Obtener todos los alumnos, paginable
+    @GetMapping("/all")
+    public ResponseEntity<?> listado(
+            @RequestParam(required = false, name = "nombre") Optional<String> nombre,
+            @RequestParam(required = false, name = "correo") Optional<String> correo,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Pageable paging = PageRequest.of(page, size);
+        Page<Alumno> pagedResult;
+        try {
+            if (nombre.isPresent()) {
+                pagedResult = alumnoRepository.findByNombreContainsIgnoreCase(nombre.get(), paging);
+            } else if (correo.isPresent()){
+                pagedResult = alumnoRepository.findByNombreContainsIgnoreCase(correo.get(), paging);
+            } else {
+                pagedResult = alumnoRepository.findAll(paging);
+            }
+            List<Alumno> alumnos = pagedResult.getContent();
+            ListAlumnoPageDTO listAlumnoPageDTO = ListAlumnoPageDTO.builder()
+                    .data(alumnoMapper.toDTO(alumnos))
+                    .totalPages(pagedResult.getTotalPages())
+                    .totalElements(pagedResult.getTotalElements())
+                    .currentPage(pagedResult.getNumber())
+                    .build();
+            return ResponseEntity.ok(listAlumnoPageDTO);
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Selección de datos", "Parámetros de consulta incorrectos");
+        }
+    }
+}
---
+++ src/main/java/es/mendoza/fpspringbootrest/controllers/modulo/ModuloAuthRestController.java
@@ -0,0 +1,157 @@
+package es.mendoza.fpspringbootrest.controllers.modulo;
+
+import es.mendoza.fpspringbootrest.config.APIConfig;
+import es.mendoza.fpspringbootrest.dto.modulos.CreateModuloDTO;
+import es.mendoza.fpspringbootrest.dto.modulos.ListModuloPageDTO;
+import es.mendoza.fpspringbootrest.errors.GeneralBadRequestException;
+import es.mendoza.fpspringbootrest.errors.modulos.ModuloBadRequestException;
+import es.mendoza.fpspringbootrest.errors.modulos.ModuloNotFoundException;
+import es.mendoza.fpspringbootrest.errors.modulos.ModulosNotFoundException;
+import es.mendoza.fpspringbootrest.mapper.ModuloMapper;
+import es.mendoza.fpspringbootrest.models.Modulo;
+import es.mendoza.fpspringbootrest.repositories.ModuloRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(APIConfig.API_PATH + "/auth/modulo")
+public class ModuloAuthRestController {
+    private final ModuloRepository moduloRepository;
+    private final ModuloMapper moduloMapper;
+
+    public ModuloAuthRestController(ModuloRepository moduloRepository, ModuloMapper moduloMapper) {
+        this.moduloRepository = moduloRepository;
+        this.moduloMapper = moduloMapper;
+    }
+
+    @CrossOrigin(origins = "http://localhost:7575")
+
+    //Obtenemos todos los módulos
+    @GetMapping("/")
+    public ResponseEntity<?> findAll(@RequestParam(name = "limit") Optional<String> limit, @RequestParam(name = "nombre") Optional<String> nombre) {
+        List<Modulo> modulos = null;
+        try {
+            if (nombre.isPresent()) {
+                modulos = moduloRepository.findByNombreContainsIgnoreCase(nombre.get());
+            } else {
+                modulos = moduloRepository.findAll();
+            }
+            if (limit.isPresent() && !modulos.isEmpty() && modulos.size() > Integer.parseInt(limit.get())) {
+                return ResponseEntity.ok(moduloMapper.toDTO(modulos.subList(0, Integer.parseInt(limit.get()))));
+            } else {
+                if (!modulos.isEmpty()) {
+                    return ResponseEntity.ok(moduloMapper.toDTO(modulos));
+                } else {
+                    throw new ModulosNotFoundException();
+                }
+            }
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Selección de datos", "Parámetros de consulta incorrectos");
+        }
+    }
+
+    //Obtenemos un módulo por id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Modulo modulo = moduloRepository.findById(id).orElse(null);
+        if (modulo == null) {
+            throw new ModuloNotFoundException(id);
+        } else {
+            return ResponseEntity.ok(moduloMapper.toDTO(modulo));
+        }
+    }
+
+    //Insertar un módulo
+    @PostMapping("/")
+    public ResponseEntity<?> save(@RequestBody CreateModuloDTO moduloDTO) {
+        try {
+            Modulo modulo = moduloMapper.fromDTO(moduloDTO);
+            checkModuloData(modulo);
+            Modulo moduloInsertado = moduloRepository.save(modulo);
+            return ResponseEntity.ok(moduloMapper.toDTO(moduloInsertado));
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Insertar", "Error al insertar el módulo. Campos incorrectos");
+        }
+    }
+
+    //Actualizando modulo por id
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Modulo modulo) {
+        try {
+            Modulo moduloActualizado = moduloRepository.findById(id).orElse(null);
+            if (moduloActualizado == null) {
+                throw new ModuloNotFoundException(id);
+            } else {
+                checkModuloData(modulo);
+                moduloActualizado.setNombre(modulo.getNombre());
+                moduloActualizado.setSiglas(modulo.getSiglas());
+                moduloActualizado = moduloRepository.save(moduloActualizado);
+                return ResponseEntity.ok(moduloMapper.toDTO(moduloActualizado));
+            }
+        } catch (ResponseStatusException e) {
+            throw new GeneralBadRequestException("Actualizar", "Error al actualizar el módulo. Campos incorrectos");
+        }
+    }
+
+    //Borrar un módulo
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            Modulo modulo = moduloRepository.findById(id).orElse(null);
+            if (modulo == null) {
+                throw new ModuloNotFoundException(id);
+            } else {
+                moduloRepository.delete(modulo);
+                return ResponseEntity.ok(moduloMapper.toDTO(modulo));
+            }
+        } catch (ResponseStatusException e) {
+            throw new GeneralBadRequestException("Eliminar", "Error al borrar el curso");
+        }
+    }
+
+    private void checkModuloData(Modulo modulo) {
+        if (modulo.getNombre() == null || modulo.getNombre().isEmpty()) {
+            throw new ModuloBadRequestException("Nombre", "El nombre es obligatorio");
+        }
+        if (modulo.getSiglas() == null || modulo.getSiglas().isEmpty()) {
+            throw new ModuloBadRequestException("Siglas", "Las siglas son obligatorias");
+        }
+    }
+
+    //Obtener todos los módulos, paginable
+    @GetMapping("/all")
+    public ResponseEntity<?> listado(
+            @RequestParam(required = false, name = "nombre") Optional<String> nombre,
+            @RequestParam(required = false, name = "siglas") Optional<String> siglas,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Pageable paging = PageRequest.of(page, size);
+        Page<Modulo> pagedResult;
+        try {
+            if (nombre.isPresent()) {
+                pagedResult = moduloRepository.findByNombreContainsIgnoreCase(nombre.get(), paging);
+            } else if (siglas.isPresent()) {
+                pagedResult = moduloRepository.findByNombreContainsIgnoreCase(siglas.get(), paging);
+            } else {
+                pagedResult = moduloRepository.findAll(paging);
+            }
+            ListModuloPageDTO listModuloPageDTO = ListModuloPageDTO.builder()
+                    .data(moduloMapper.toDTO(pagedResult.getContent()))
+                    .totalPages(pagedResult.getTotalPages())
+                    .totalElements(pagedResult.getTotalPages())
+                    .currentPage(pagedResult.getNumber())
+                    .build();
+            return ResponseEntity.ok(listModuloPageDTO);
+        } catch (Exception e) {
+            throw new GeneralBadRequestException("Selección de datos", "Parámetros de consulta incorrectos");
+        }
+    }
+}
